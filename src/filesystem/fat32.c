a7a3365cb58d2aae10c7196dc8c56723e2f960a5
Zechtro <stvn.tjhia@gmail.com>
2024-03-11 00:09:21 +0700

    feat:initialize FAT32

--- src/filesystem/fat32.c
+++ src/filesystem/fat32.c
@@ -5,13 +5,7 @@
 #include "../header/filesystem/fat32.h"
 
 const uint8_t fs_signature[BLOCK_SIZE] = {
-    'C', 'o', 'u', 'r', 's', 'e', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',  ' ',
-    'D', 'e', 's', 'i', 'g', 'n', 'e', 'd', ' ', 'b', 'y', ' ', ' ', ' ', ' ',  ' ',
-    'L', 'a', 'b', ' ', 'S', 'i', 's', 't', 'e', 'r', ' ', 'I', 'T', 'B', ' ',  ' ',
-    'M', 'a', 'd', 'e', ' ', 'w', 'i', 't', 'h', ' ', '<', '3', ' ', ' ', ' ',  ' ',
-    '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2', '0', '2', '4', '\n',
-    [BLOCK_SIZE-2] = 'O',
-    [BLOCK_SIZE-1] = 'k',
+    'z','e','n','O','S',' ','h','a','s',' ','b','o','o','t','e','d'
 };
 
 struct FAT32DriverState driverState;
@@ -22,7 +16,19 @@ uint32_t cluster_to_lba(uint32_t cluster){
 
 // void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uint32_t parent_dir_cluster);
 
-// bool is_empty_storage(void);
+bool is_empty_storage(void){
+    struct BlockBuffer block;
+    read_blocks(block.buf,BOOT_SECTOR,1);
+    bool isEmpty = false;
+    int i = 0;
+    while(i<BLOCK_SIZE and !isEmpty){
+        if(block.buf[i] != fs_signature[i]){
+            isEmpty = true;
+        i++;
+        }
+    }
+    return isEmpty;
+}
 
 void create_fat32(void){
     write_blocks(fs_signature,BOOT_SECTOR,1);
@@ -36,7 +42,14 @@ void create_fat32(void){
     write_clusters(&driverState.fat_table,1,1);
 };
 
-// void initialize_filesystem_fat32(void);
+void initialize_filesystem_fat32(void){
+    if (is_empty_storage()){
+        create_fat32();
+    }
+    else{
+        read_clusters(&driverState.fat_table,1,1);
+    }
+}
 
 void write_clusters(const void *ptr, uint32_t cluster_number, uint8_t cluster_count){
     write_blocks(ptr,cluster_to_lba(cluster_number),CLUSTER_BLOCK_COUNT*cluster_count);
