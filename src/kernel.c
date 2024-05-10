b64fd5b5d35b56f4211e44be6e3c48f36a87a968
riyorax <maximiliansulistiyo@gmail.com>
2024-05-10 18:25:35 +0700

    fix :fixed user mode

--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,16 @@
 #include <stdint.h>
 #include <stdbool.h>
-#include "header/cpu/gdt.h"
-#include "header/kernel-entrypoint.h"
-#include "header/framebuffer.h"
-#include "header/interrupt/idt.h"
-#include "header/interrupt/interrupt.h"
+#include "./header/cpu/gdt.h"
+#include "./header/kernel-entrypoint.h"
+#include "./header/framebuffer.h"
+#include "./header/interrupt/idt.h"
+#include "./header/interrupt/interrupt.h"
 #include "./header/driver/disk.h"
-#include "header/filesystem/fat32.h"
+#include "./header/filesystem/fat32.h"
 #include "./header/stdlib/string.h"
-#include "header/keyboard.h"
-#include "header/stdlib/string.h"
-#include "header/memory/paging.h"
+#include "./header/keyboard.h"
+#include "./header/stdlib/string.h"
+#include "./header/memory/paging.h"
 
 // void kernel_setup(void) {
 //     uint32_t a;
@@ -103,24 +103,6 @@
 // }
 
 // Keyboard Test
-void kernel_setup(void) {
-    load_gdt(&_gdt_gdtr);
-    pic_remap();
-    initialize_idt();
-    activate_keyboard_interrupt();
-    framebuffer_clear();
-    framebuffer_set_cursor(0, 0);
-        
-    int col = 0;
-    keyboard_state_activate();
-    while (true) {
-         char c;
-         get_keyboard_buffer(&c);
-         framebuffer_write(0, col++, c, 0xF, 0);
-    }
-}
-
-// Ini buat bab 2
 // void kernel_setup(void) {
 //     load_gdt(&_gdt_gdtr);
 //     pic_remap();
@@ -128,27 +110,45 @@ void kernel_setup(void) {
 //     activate_keyboard_interrupt();
 //     framebuffer_clear();
 //     framebuffer_set_cursor(0, 0);
-//     initialize_filesystem_fat32();
-//     gdt_install_tss();
-//     set_tss_register();
-
-//     // Allocate first 4 MiB virtual memory
-//     paging_allocate_user_page_frame(&_paging_kernel_page_directory, (uint8_t*) 0);
-
-//     // Write shell into memory
-//     struct FAT32DriverRequest request = {
-//         .buf                   = (uint8_t*) 0,
-//         .name                  = "shell",
-//         .ext                   = "\0\0\0",
-//         .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-//         .buffer_size           = 0x100000,
-//     };
-//     read(request);
-
-//     // Set TSS $esp pointer and jump into shell 
-//     set_tss_kernel_current_stack();
-//     kernel_execute_user_program((uint8_t*) 0);
-
-//     while (true);
+        
+//     int col = 0;
+//     keyboard_state_activate();
+//     while (true) {
+//          char c;
+//          get_keyboard_buffer(&c);
+//          framebuffer_write(0, col++, c, 0xF, 0);
+//     }
 // }
+
+// Ini buat bab 2
+void kernel_setup(void) {
+    load_gdt(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
+    activate_keyboard_interrupt();
+    framebuffer_clear();
+    framebuffer_set_cursor(0, 0);
+    initialize_filesystem_fat32();
+    gdt_install_tss();
+    set_tss_register();
+
+    // Allocate first 4 MiB virtual memory
+    paging_allocate_user_page_frame(&_paging_kernel_page_directory, (uint8_t*) 0);
+
+    // Write shell into memory
+    struct FAT32DriverRequest request = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = "shell",
+        .ext                   = "\0\0\0",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size           = 0x100000,
+    };
+    read(request);
+
+    // Set TSS $esp pointer and jump into shell 
+    set_tss_kernel_current_stack();
+    kernel_execute_user_program((uint8_t*) 0);
+
+    while (true);
+}
 
