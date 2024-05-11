8d600fb634ced68bcce68eecb276bac57982fe30
riyorax <maximiliansulistiyo@gmail.com>
2024-05-11 14:09:54 +0700

    feat : finished cli prompt

--- src/header/user-shell.h
+++ src/header/user-shell.h
@@ -3,36 +3,44 @@
 #define SHELL_DIRECTORY "user@zenOS:~"
 #define SHELL_PROMPT "$ "
 
-#define BIOS_BLACK         0x0
-#define BIOS_BLUE          0x1
-#define BIOS_GREEN         0x2
-#define BIOS_CYAN          0x3
-#define BIOS_RED           0x4
-#define BIOS_MAGENTA       0x5
-#define BIOS_BROWN         0x6
-#define BIOS_GRAY          0x7
+#define BIOS_BLACK 0x0
+#define BIOS_BLUE 0x1
+#define BIOS_GREEN 0x2
+#define BIOS_CYAN 0x3
+#define BIOS_RED 0x4
+#define BIOS_MAGENTA 0x5
+#define BIOS_BROWN 0x6
+#define BIOS_GRAY 0x7
 
-#define BIOS_DARK_GRAY     0x8
-#define BIOS_LIGHT_BLUE    0x9
-#define BIOS_LIGHT_GREEN   0xA
-#define BIOS_LIGHT_CYAN    0xB
-#define BIOS_LIGHT_RED     0xC
+#define BIOS_DARK_GRAY 0x8
+#define BIOS_LIGHT_BLUE 0x9
+#define BIOS_LIGHT_GREEN 0xA
+#define BIOS_LIGHT_CYAN 0xB
+#define BIOS_LIGHT_RED 0xC
 #define BIOS_LIGHT_MAGENTA 0xD
-#define BIOS_YELLOW        0xE
-#define BIOS_WHITE         0xF
+#define BIOS_YELLOW 0xE
+#define BIOS_WHITE 0xF
 
-struct location{
+struct location
+{
     uint8_t row;
     uint8_t col;
 };
 
-struct shellState{
+struct shellState
+{
     uint32_t workDir;
     char commandBuffer[256];
     uint8_t bufferIndex;
     struct location startingWriteLoc;
     char directory[256];
 };
+
+void syscall(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx);
+
+void reset_shell_buffer();
+
+void process_commands();
 
 void print_shell_prompt();
 
